c2cdd57f19eb4f3a2d29ddea0e3ce0d4bb092760
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-11-04 22:24:41 +0100

    ex2

--- tableaux/ex2/ex2.c
+++ tableaux/ex2/ex2.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int tab[50], n;
-
-    printf("entrer le nombre de tab: ",n);
-    for (int i = 0; i < n; i++)
-    {
-        printf("saisir les elements: ");
+    int n;
+    int tab[50];
+    printf("entrer le nombre de tableau:");
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        printf("entrer element nr %d: ", +i);
         scanf("%d",&tab[i]);
     }
-    printf("les elements u tableaux:");
-    
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d",tab[i]);
+    for(int i = 0; i < n; i++){
+        printf("%d ",tab[i]);
     }
-    
 }
