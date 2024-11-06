00a1293246c58b40df59bc1837fbd87124701d26
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-11-06 22:08:23 +0100

    ex4

---
+++ tableaux/ex4/e4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int n, tab[50];
+    int max;
+    printf("entrer la valeur de tableau: ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("entrer les valeurs: ", +i);
+        scanf("%d",&tab[i]);
+    }
+    max = tab[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(tab[i] > max){
+            max = tab[i];
+        }
+    }
+    printf("%d",max);
+    printf("\n");
+    
+}
