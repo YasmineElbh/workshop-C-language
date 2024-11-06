b9ca2a540fb24006625272e8e2db57c4598c1a0d
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-11-06 22:12:52 +0100

    ex5

---
+++ tableaux/ex5/e5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int n, tab[50];
+
+    int min;
+    printf("entrer la valeur de tableau: ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("entrer les valeurs: ", +i);
+        scanf("%d",&tab[i]);
+    }
+    min = tab[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(tab[i] < min){
+            min = tab[i];
+        }
+    }
+    printf("%d",min);
+    printf("\n");
+}
