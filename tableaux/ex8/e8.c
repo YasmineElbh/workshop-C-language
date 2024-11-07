a410f62595aedfa915e0eadc7c39a17be53247b4
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-11-07 22:36:25 +0100

    ex8

--- tableaux/ex8/e8.c
+++ tableaux/ex8/e8.c
@@ -6,25 +6,30 @@ int main(){
 
     printf("entrer le nombre de tableau: ");
     scanf("%d",&n);
+    int tab2[n];
+
+
     for(int i = 0; i < n; i++){
         printf("entrer element %d: ", +i);
         scanf("%d",&tab[i]);
     }
-    int swap = 0;
-    for (int i = 0; i < n - 1; i++)
+    
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            tmp = tab[j];
-            tab[j] = tab[j+1];
-            tab[j+1] = tmp;
-            swap = 1;
-        }
+        tab2[i] = tab[i];
+        // for (int j = 0; j < n - i - 1; j++)
+        // {
+        //     tmp = tab[i];
+        //     tmp = tab[j];
+        //     tab[j] = tab[j+1];
+        //     tab[j+1] = tmp;
+            
+        // }
         
     }
-    for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",tab[j]);
+        printf("%d ",tab2[i]);
     }
 
 }
