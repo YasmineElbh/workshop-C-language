45ce979a2f22494d8758bdfd06764d2e19503d25
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-11-11 23:32:06 +0100

    ex11

---
+++ tableaux/ex11/e11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int i, tab[50], n;
+
+    printf("entrer le nombre de tableaux: ");
+    scanf("%d",&n);
+
+    for(int i = 0; i < n; i++){
+        printf("entrer element nr %d: ", i);
+        scanf("%d",&tab[i]);
+    }
+
+    int found = 0;
+    
+}
