0e2f367ccb6123729c6aed6310f65f5d186e6645
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-12-01 15:26:05 +0100

    ex11

--- tableaux/ex11/e11.c
+++ tableaux/ex11/e11.c
@@ -16,19 +16,19 @@ int main(){
     
 
     int found = 0;
-    printf("entrer element que voulew vous remplace avec: ");
+    printf("entrer element que voulez vous remplace avec: ");
     scanf("%d",&replace);
     for(int i = 0; i < n; i++){
         
         if(tab[i] == r)
         {
             tab[i] = replace;
-            printf("vous avez remplace %d avec %d",r,replace,i);
+            printf("vous avez remplace %d avec %d\n",r,replace,i);
             found = 1;
             
         }
     }
     for(int i = 0; i < n; i++){
-        printf("%d",tab[i]);
+        printf("%d\n",tab[i]);
     }
 }
