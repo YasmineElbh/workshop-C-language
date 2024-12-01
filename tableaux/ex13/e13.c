d0e18e1abf72f2b25c23a8d441a42a9277624ed1
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-12-01 21:13:10 +0100

    ex13

---
+++ tableaux/ex13/e13.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int i, tab[50], n;
+    int r, replace;
+    printf("entrer le nombre de tableaux: ");
+    scanf("%d",&n);
+
+    for(int i = 0; i < n; i++){
+        printf("entrer element nr %d: ", i);
+        scanf("%d",&tab[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        if(tab[i] % 2 != 0){
+            printf("le paire est: %d\n", tab[i]);
+        }
+    }
+}
